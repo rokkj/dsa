9bfed9310e07cc8ceb5089d1b5de90aeb7fca32f
ronak.jain <jvronak@gmail.com>
2024-06-06 16:15:42 +0530

    append and pop

--- python/elementary/LinkedList/constructor.py
+++ python/elementary/LinkedList/constructor.py
@@ -16,5 +16,35 @@ class LinkedList:
             print(temp.value)
             temp= temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head.next is None:
+            self.head.next = new_node
+            self.tail.next = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
 my_linkedlist = LinkedList(4)
+my_linkedlist.append(7)
+my_linkedlist.pop()
 my_linkedlist.printlist()
