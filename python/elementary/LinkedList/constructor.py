338923ffcb1490e2a0983d5f856276b7e0648d89
ronak.jain <jvronak@gmail.com>
2024-06-06 18:35:14 +0530

    pop_first

--- python/elementary/LinkedList/constructor.py
+++ python/elementary/LinkedList/constructor.py
@@ -44,7 +44,30 @@ class LinkedList:
             self.head = None
             self.tail = None
 
-my_linkedlist = LinkedList(4)
-my_linkedlist.append(7)
-my_linkedlist.pop()
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head == new_node
+            self.tail == new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+my_linkedlist = LinkedList(10)
+my_linkedlist.append(11)
+my_linkedlist.printlist()
+my_linkedlist.pop_first()
 my_linkedlist.printlist()
