7ab77b422536150effc720117723262527ae5868
ronak.jain <jvronak@gmail.com>
2024-06-08 00:11:56 +0530

    insert/ remove

--- python/elementary/linkedlist.py
+++ python/elementary/linkedlist.py
@@ -81,6 +81,20 @@ class LinkedList:
             return True
         return False
 
+        def insert(self, index, value):
+            if index < 0 or index > self.length:
+                return False
+            if index == 0:
+                return self.prepend(value)
+            if index == self.length:
+                return self.append(value)
+            new_node = Node(value)
+            temp = self.get(index - 1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1   
+            return True
+    
 my_linkedlist = LinkedList(1)
 my_linkedlist.append(2)
 my_linkedlist.printlist()
