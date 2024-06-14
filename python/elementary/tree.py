6e39e67f90d69db495104ce82914737db06c801d
ronak.jain <jvronak@gmail.com>
2024-06-14 17:39:20 +0530

    binary tree

---
+++ python/elementary/tree.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while(True):
+            if temp.value == new_node.value:
+                return False
+            if temp.value < new_node.value:
+                if temp.left == None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right == None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+my_tree = BinarySearchTree()
+print(my_tree.root)
