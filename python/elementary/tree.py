f1bb328c916a4c194aac14a3699d4e7036a4ce2a
ronak.jain <jvronak@gmail.com>
2024-06-15 15:12:31 +0530

    binary and rbst

--- python/elementary/tree.py
+++ python/elementary/tree.py
@@ -27,12 +27,41 @@ class BinarySearchTree:
                     temp.right = new_node
                     return True
                 temp = temp.right
+    
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                print(temp.value)
+                return temp
+        return False
+
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+
+    def __r_contains(self, current_node, value):
+        if self.root == None:
+            return False
+        if self.root.value == value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+
+
 
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+my_tree.r_contains(18)
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
