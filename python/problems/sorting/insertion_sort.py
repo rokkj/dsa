ea6010ea87a3ad8a0f0575e1b0bd1d0d82dd5ffb
ronak.jain <jvronak@gmail.com>
2024-08-19 15:30:23 +0530

    Insertion Sort refactor

--- python/problems/sorting/insertion_sort.py
+++ python/problems/sorting/insertion_sort.py
@@ -1,19 +1,14 @@
-
 def insertion_sort(arr):
-    """
-    Args:
-     arr(list_int32)
-    Returns:
-     list_int32
-    """
-    # Write your code here.
     n = len(arr)
+
     for i in range(1, n):
-        temp = arr[i]
-        j = i-1
-        while j > -1 and arr[j] > temp:
-            arr[j+1] = arr[j]
-            arr[j] = temp
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
             j -= 1
-            
+        
+        arr[j + 1] = key
+
     return arr
